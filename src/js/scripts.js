cb8d6b04895228abaae60d0f86508a0d1d951541
Maros Korinek <maros.korinek@fundingcircle.com>
2016-10-19 10:05:55 +0100

    WIP

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,16 @@
 // TODO: THIS JS NEEDS CLEANUP
 
-// function scroll_to(clicked_link, nav_height) {
-// 	var element_class = clicked_link.attr('href').replace('#', '.');
-// 	var scroll_to = 0;
-// 	if(element_class != '.top-content') {
-// 		element_class += '-container';
-// 		scroll_to = $(element_class).offset().top - nav_height;
-// 	}
-// 	if($(window).scrollTop() != scroll_to) {
-// 		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
-// 	}
-// }
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
 
 
 jQuery(document).ready(function() {
