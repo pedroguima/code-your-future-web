1d9af7e975eef65f3d0d938c2fb45d0876a65cf9
Maros Korinek <maros.korinek@fundingcircle.com>
2016-11-12 09:38:04 +0000

    Cleanup js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,4 @@
-// TODO: THIS JS NEEDS CLEANUP
-
-function scroll_to(clicked_link, nav_height) {
-	var element_class = clicked_link.attr('href').replace('#', '.');
-	var scroll_to = 0;
-	if(element_class != '.top-content') {
-		element_class += '-container';
-		scroll_to = $(element_class).offset().top - nav_height;
-	}
-	if($(window).scrollTop() != scroll_to) {
-		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
-	}
-}
-
-
 jQuery(document).ready(function() {
-
-	/*
-	    Navigation
-	*/
-	$('a.scroll-link').on('click', function(e) {
-		e.preventDefault();
-		scroll_to($(this), $('nav').outerHeight());
-	});
 
     /*
 	    Scroll to top
@@ -35,93 +12,7 @@ jQuery(document).ready(function() {
 	});
 
     /*
-	    Wow
-	*/
-	new WOW().init();
-
-    /*
         Fullscreen backgrounds
     */
     $('.page-title').backstretch("img/backgrounds/1.jpg");
-    $('.students-title').backstretch("img/backgrounds/2.jpg");
-    // $('.counters-container').backstretch("img/backgrounds/1.jpg");
-    $('.section-hero-container').backstretch("img/backgrounds/1.jpg");
-    // $('.call-to-action-container').backstretch("img/backgrounds/1.jpg");
-
-	/*
-	    Counters
-	*/
-	$('.counters-container').waypoint(function() {
-		$('.counter-box h4').countTo();
-	}, { offset: '100%' });
-
-	/*
-	    Testimonials
-	*/
-	$('.testimonial-active').html('<p>' + $('.testimonial-single:first p').html() + '</p>');
-	$('.testimonial-single:first .testimonial-single-image img').css('opacity', '1');
-
-	$('.testimonial-single-image img').on('click', function() {
-		$('.testimonial-single-image img').css('opacity', '0.3');
-		$(this).css('opacity', '1');
-		var new_testimonial_text = $(this).parent('.testimonial-single-image').siblings('p').html();
-		$('.testimonial-active p').fadeOut(300, function() {
-			$(this).html(new_testimonial_text);
-			$(this).fadeIn(400);
-		});
-	});
-
-	/*
-	    Contact form
-	*/
-	$('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
-		$('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
-	});
-	$('.contact-form form').submit(function(e) {
-		e.preventDefault();
-	    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
-	    var postdata = $('.contact-form form').serialize();
-	    $.ajax({
-	        type: 'POST',
-	        url: 'assets/contact.php',
-	        data: postdata,
-	        dataType: 'json',
-	        success: function(json) {
-	            if(json.emailMessage != '') {
-	                $('.contact-form form .contact-email').addClass('contact-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-            			$(this).removeClass('animated shake');
-            		});
-	            }
-	            if(json.subjectMessage != '') {
-	                $('.contact-form form .contact-subject').addClass('contact-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-            			$(this).removeClass('animated shake');
-            		});
-	            }
-	            if(json.messageMessage != '') {
-	                $('.contact-form form textarea').addClass('contact-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-            			$(this).removeClass('animated shake');
-            		});
-	            }
-	            if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
-	                $('.contact-form form').fadeOut('fast', function() {
-	                    $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
-	                });
-	            }
-	        }
-	    });
-	});
-
-});
-
-
-
-jQuery(window).load(function() {
-
-	/*
-		Loader
-	*/
-	$(".loader-img").fadeOut();
-	$(".loader").delay(1000).fadeOut("slow");
-
-
 });
