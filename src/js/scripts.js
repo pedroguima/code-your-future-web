70d223f1fc0e15ddce5b1b667096f742c3c89992
Maros Korinek <maros.korinek@fundingcircle.com>
2017-01-22 11:51:49 +0000

    Refactor background images to use mixins

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,18 +1,12 @@
 jQuery(document).ready(function() {
-
-    /*
-	    Scroll to top
-	*/
-    $('.scroll-to-top a').on('click', function(e) {
-		e.preventDefault();
-		var scroll_to = 0;
-		if($(window).scrollTop() != scroll_to) {
-			$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
-		}
-	});
-
-    /*
-        Fullscreen backgrounds
-    */
-    $('.page-title').backstretch("img/backgrounds/1.jpg");
+  /*
+   Scroll to top
+   */
+  $('.scroll-to-top a').on('click', function(e) {
+    e.preventDefault();
+    var scroll_to = 0;
+    if($(window).scrollTop() != scroll_to) {
+      $('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+    }
+  });
 });
