a6ebcd52d5a8567c41ed3a745661006b853acb39
Pedro Guimaraes <pedro.guima@gmail.com>
2017-02-01 23:00:08 +0000

    testing stuff

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ git clone git@github.com:pedroguima/$REPO_NAME.git
 cd $REPO_NAME
 git rm *
 cp -a ../build/* .
-git add .
-git commit -m "Rebuild on $DATE"
 git config --global user.email "bot@codeyourfuture.co"
 git config --global user.name "Automated bot"
+git add .
+git commit -m "Rebuild on $DATE"
 git push
